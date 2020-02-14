1dfde8f719fd9a703cf2c8ad02694865000390fb
Augustinu04 <st04pvp@gmail.com>
2020-02-14 14:22:56 +0200

    Version 1
    first version of the game

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag=="Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            Data.banuti++;
+        }
+    }
+}
+
